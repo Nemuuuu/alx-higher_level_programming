f24ed70cbbaaced85c56558b29f95326701e3e01
Nemuuuu <111045567+Nemuuuu@users.noreply.github.com>
2022-11-29 18:43:18 +0100

    Delete 9-print_last_digit.py

--- 9-print_last_digit.py
+++
@@ -1,8 +0,0 @@
-def print_last_digit(number):
-    if number < 0:
-        return -1 * (number % 10)
-    else:
-        return number % 10
-print(print_last_digit(98))
-print(print_last_digit(0))
-print(print_last_digit(-1024))
