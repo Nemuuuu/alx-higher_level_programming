cc54a145e8f272a03b734b4586cc9d1fcea24abf
Nemuuuu <111045567+Nemuuuu@users.noreply.github.com>
2022-11-29 18:35:28 +0100

    '

--- 0-positive_or_negative.py
+++
@@ -1,8 +0,0 @@
-import random
-number = random.randint(-10, 10)
-if number > 0:
-    print("%d is positive\n"%(number));
-elif number < 0:
-    print("%d is negative\n"%(number));
-else:
-    print("%d is zero\n"%(number));
