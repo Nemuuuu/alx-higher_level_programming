5143eae14054aa1be1fcb35d1556aa5c0c5b04ac
Nemuuuu <111045567+Nemuuuu@users.noreply.github.com>
2022-12-01 22:32:58 +0300

    updated

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import calculator_1
+a = 10
+b = 5
+print("%i + %i = %i"%(a, b, calculator_1.add(a,b)))
+print("%i - %i = %i"%(a, b, calculator_1.sub(a,b)))
+print("%i * %i = %i"%(a, b, calculator_1.mul(a,b)))
+print("%i / %i = %i"%(a, b, calculator_1.div(a,b)))
