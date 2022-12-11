896fe12c8151b87782cd71e1f521eaca2e4f3b7d
Neima Nesru <neimanesru34@gmail.com>
2022-12-11 12:45:44 +0000

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python3
 import sys
 i = 1
 sum = 0
 if len(sys.argv) == 1:
-    print(f"{len(sys.argv) - 1}")
+    print("{}".format(len(sys.argv) - 1))
 else:
     while i < len(sys.argv):
         sum += int(sys.argv[i])
-    i += 1
+    	i += 1
+	print("{}".format(sum))
+
