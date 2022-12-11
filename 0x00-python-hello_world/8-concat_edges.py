dd65eed5aee8885e3fd86418384d5d786798a408
Nemuuuu <111045567+Nemuuuu@users.noreply.github.com>
2022-12-11 14:46:40 +0100

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,2 +1,5 @@
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(f"{str[6:8]} with Python")
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
