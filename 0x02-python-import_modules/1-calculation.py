1a47277a84ccf5d671095b38fe08304862bcac50
Neima Nesru <neimanesru34@gmail.com>
2022-12-11 12:32:26 +0000

    file update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
-
+#!/usr/bin/python3
 
 import calculator_1
 if __name__ == '__main__':
     a = 10
     b = 5
-    print("%i + %i = %i"%(a, b, calculator_1.add(a,b)))
-    print("%i - %i = %i"%(a, b, calculator_1.sub(a,b)))
-    print("%i * %i = %i"%(a, b, calculator_1.mul(a,b)))
-    print("%i / %i = %i"%(a, b, calculator_1.div(a,b)))
+    print("{} + {} = {}".format(a, b,calculator_1.add(a, b)))
+    print("{} - {} = {}".format(a, b, calculator_1.sub(a,b)))
+    print("{} * {} = {}".format(a, b, calculator_1.mul(a,b)))
+    print("{} / {} = {}".format(a, b, calculator_1.div(a,b)))
