fadb17d7e262b41a6a2148e2a389b1b829dcddda
Nemuuuu <111045567+Nemuuuu@users.noreply.github.com>
2022-12-11 14:56:40 +0100

    Update 2-unique_add.py

--- 0x04-python-more_data_structures/2-unique_add.py
+++ 0x04-python-more_data_structures/2-unique_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    uniq = [num for num in my_list if my_list.count(num) > 1]
-    duplic = list(set(uniq))
-    unique = list(set(my_list))
-    sum = 0
-    for i in unique:
-        sum += i
-        
-    return sum
+    new = set(my_list)
+    res = 0
+    for i in new:
+        res += i
+    return 
