578b41c0abd2131657aa5b4a08ce7318f52ed301
Neima Nesru <neimanesru34@gmail.com>
2022-12-11 12:21:54 +0000

    file updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,13 @@ import sys
 if __name__ == '__name__':
     i = 1
     if i >= len(sys.argv):
-        print(f"0 arguments.")
+        print("0 arguments.")
     else:
         while i >= len(sys.argv):
             if len(sys.argv) == 2:
-                print(f"{len(sys.argv) - 1} argument: ")
-                print(f"{i}: {sys.argv[i]}")
+                print("{} argument: ".format(len(sys.argv) - 1))
+                print("{}: {}".format(i,sys.argv[i]))
             else:
-                print(f"{len(sys.argv) - 1} arguments: ")
-                print(f"{i}: {sys.argv[i]}")
+                print("{} arguments: ".format(len(sys.argv) - 1))
+                print("{}: {}".format(i,sys.argv[i]))
         i +=1
