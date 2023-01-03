7a545d397c176d8200053f19a2e73952b961b784
root <root@DESKTOP-FJJNQBP.localdomain>
2023-01-03 12:16:08 +0100

    test cases are added

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,2 +1,6 @@
-unitest
+import unittest
 
+class TestMaxInteger:
+    def test_max_integer(unittest.TestCase):
+        self.assertEqual(max_integer([1,2,3,4]), 4)
+        self.assertEqual(max_integer([7,3,4,2]), 7)
