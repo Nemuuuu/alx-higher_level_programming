9a4c721b5772c11345b75dd0946d01e1f813182f
Nemuuuu <111045567+Nemuuuu@users.noreply.github.com>
2023-01-03 08:55:03 +0100

    updated

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" Rectangle class to represent a rectangle """
+"""Rectangle class to represent a rectangle
+"""
 
 class Rectangle:
-""" Defines a Rectangle and its basic properties """
+    """Defines a Rectangle and its basic properties
+    """
     pass
