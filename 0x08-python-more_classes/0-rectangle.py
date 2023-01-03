7e018b1b2d04b085184f76fba01a933178729dea
Neima Nesru <neimanesru34@gmail.com>
2023-01-03 07:52:56 +0000

    file updated

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,5 +2,5 @@
 """ Rectangle class to represent a rectangle """
 
 class Rectangle:
-	""" Defines a Rectangle and its basic properties """
+""" Defines a Rectangle and its basic properties """
     pass
