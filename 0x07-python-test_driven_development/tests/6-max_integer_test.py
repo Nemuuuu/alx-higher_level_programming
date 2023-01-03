5a3da0e4edd9d6f6ca14bd347a6f3a668d329002
Nemuuuu <111045567+Nemuuuu@users.noreply.github.com>
2023-01-03 12:20:11 +0100

    Delete 0x07-python-test_driven_development directory

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-class TestMaxInteger:
-    def test_max_integer(unittest.TestCase):
-        self.assertEqual(max_integer([1,2,3,4]), 4)
-        self.assertEqual(max_integer([7,3,4,2]), 7)
